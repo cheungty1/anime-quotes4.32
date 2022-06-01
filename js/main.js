201ce7e6d0013e4bc507b88f51a4aae34e476b92
cheungty1 <99298338+cheungty1@users.noreply.github.com>
2022-06-01 13:41:20 +1000

    master

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-//Version: 4.3
-//Last Edited: 23/05/22
+//Version: 4.34
+//Last Edited: 26/05/22
 //Edited by: Tait
 
 //This Script connects to the animechan.vercel.app API and returns famous quotes of the anime searched by the user.
@@ -13,17 +13,26 @@ const submitBtn = document.getElementById("submit")
 const alertBox= document.querySelector('.alert')
 const factsDiv = document.getElementById("facts");
 
+//Added an addEventListener to the window object so that the default display of the error alertBox is hidden when the page first loads.
+//Added LocalStorage getItem, so that browser will auto-load old search result when refresh page
+window.addEventListener("load", function () {
+  alertBox.style.display = "none"
+  const saveSearch = localStorage.getItem('search')
+  if (saveSearch) {
+    searchBox.value = saveSearch;
+    randomFact(saveSearch);
+  }
+})
+
 //Added an addEventListener to make search box dynamic, with which users' search for the quotes of their desired favourite anime.
+//Added LocalStorage setItem, so that browser will save current search result
 submitBtn.addEventListener("click", function (event) {
     event.preventDefault()
     let query = searchBox.value
     randomFact(query)
+    localStorage.setItem('search', searchBox.value)
   })
 
-//Added an addEventListener to the window object so that the default display of the error alertBox is hidden when the page first loads.
-window.addEventListener("load", function () {
-    alertBox.style.display = "none"
-  })
 
 //Connects to the API and fetches/gets the information.
 async function randomFact(query) {
